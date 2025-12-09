552168848fab5f20b4aa3522e94cdb5ed613a5bd
abccba100 <abccba111307@gmail.com>
2025-12-09 20:06:50 +0900

    init

---
+++ styles/fontStyle.ts
@@ -0,0 +1,74 @@
+export const fontStyle = {
+  semiBold24: {
+    size: "24px",
+    weight: 600
+  },
+  semiBold20: {
+    size: "20px",
+    weight: 600
+  },
+  semiBold18: {
+    size: "18px",
+    weight: 600
+  },
+  semiBold16: {
+    size: "16px",
+    weight: 600
+  },
+  semiBold14: {
+    size: "14px",
+    weight: 600
+  },
+  semiBold12: {
+    size: "12px",
+    weight: 600
+  },
+  medium24: {
+    size: "24px",
+    weight: 500
+  },
+  medium20: {
+    size: "20px",
+    weight: 500
+  },
+  medium18: {
+    size: "18px",
+    weight: 500
+  },
+  medium16: {
+    size: "16px",
+    weight: 500
+  },
+  medium14: {
+    size: "14px",
+    weight: 500
+  },
+  medium12: {
+    size: "12px",
+    weight: 500
+  },
+  regular24: {
+    size: "24px",
+    weight: 400
+  },
+  regular20: {
+    size: "20px",
+    weight: 400
+  },
+  regular18: {
+    size: "18px",
+    weight: 400
+  },
+  regular16: {
+    size: "16px",
+    weight: 400
+  },
+  regular14: {
+    size: "14px",
+    weight: 400
+  },
+  regular12: {
+    size: "12px",
+    weight: 400
+  },
+}
