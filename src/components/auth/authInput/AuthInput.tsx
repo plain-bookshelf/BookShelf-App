233a337990447f8ca736267c3aa25b99c20efa03
abc100 <abccba111307@gmail.com>
2025-12-12 23:04:34 +0900

    feat: AuthInput
    -AuthInput component 개발 완료
    -component 폴더 내 로그인 전 screen에서 공통으로 쓰일 auth 폴더 생성
    -AuthInput 에 정보 입력 시 잘못된 정보일 경우 띄울 경고 메시지 이미지 추가

---
+++ src/components/auth/authInput/AuthInput.tsx
@@ -0,0 +1,50 @@
+import Typography from "@/components/common/typography/Typography"
+import * as S from "./style"
+import { Image } from "react-native"
+import icon_input_error from "@/assets/icon_input_error.png"
+import { useState } from "react"
+
+interface AuthInputProps {
+  label: string,
+  placeholder: string,
+  size: S.InputSize,
+  isError: boolean,
+  warningMessage: string,
+  value: string,
+  onChangeText: (text: string) => void,
+}
+
+/**
+ * @param label input label 입력
+ * @param placeholder input placeholder 입력
+ * @param size input wdith 결정 small, large 중 선택
+ * @param isError 현재 warningMessage 띄울지 여부
+ * @param warningMessage 띄울 경고 메세지
+ * @param value input 현재 value
+ * @param onChangeText input 현재 value 변경
+ * @returns label과 input을 담은 하나의 요소로 반환
+ */
+export default function AuthInput({ label, placeholder, size, isError, warningMessage, value, onChangeText }: AuthInputProps) {
+ const [isFocused, setIsFocused] = useState(false);
+
+  return(
+    <S.InputBox>
+      <Typography children={label} font="semiBold18" color="defaultBlack" />
+      <S.Input
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        size={size}
+        placeholder={placeholder}
+        isError={isError} value={value}
+        isFocused={isFocused}
+        onChangeText={onChangeText}
+      />
+      {isError && !isFocused &&
+        <S.ErrorContainer size={size}>
+          <Image source={icon_input_error} style={{width: 17, height: 17}} />
+          <Typography children={warningMessage} font="regular16" color="defaultRed" />
+        </S.ErrorContainer>
+      }
+    </S.InputBox>
+  )
+}
