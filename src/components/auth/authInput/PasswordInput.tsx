4645ea6dedd52fc44f9fdc7f2c19958fd10e78bd
abc100 <abccba111307@gmail.com>
2026-02-04 10:50:38 +0900

    feat :: PasswordInput
    -password 보기, 보지 않기 이미지 추가
    -PasswordInput 개발 완료

--- src/components/auth/authInput/PasswordInput.tsx
+++ src/components/auth/authInput/PasswordInput.tsx
@@ -1,8 +1,11 @@
+import styled from "styled-components/native"
 import Typography from "@/components/common/typography/Typography"
 import * as S from "./style"
-import { Image } from "react-native"
+import { Image, Pressable } from "react-native"
 import icon_input_error from "@/assets/icon_input_error.png"
 import { useState } from "react"
+import btn_passwordShow_false from "@/assets/btn_password-show_false.png"
+import btn_passwordShow_true from "@/assets/btn_password-show_true.png"
 
 interface AuthInputProps {
   label: string,
@@ -23,20 +26,28 @@ interface AuthInputProps {
  * @param onChangeText input 현재 value 변경
  * @returns label과 input을 담은 하나의 요소로 반환
  */
-export default function DefaultInput({ label, placeholder, isError, warningMessage, value, onChangeText }: AuthInputProps) {
+export default function PasswordInput({ label, placeholder, isError, warningMessage, value, onChangeText }: AuthInputProps) {
  const [isFocused, setIsFocused] = useState(false);
+ const [passwordIsShow, setPasswordIsShow] = useState(true);
+ const passwordImage = passwordIsShow ? btn_passwordShow_true : btn_passwordShow_false;
 
   return(
     <S.InputBox>
       {label.length !== 0 && <Typography children={label} font='regular18' color='labelGray' />}
-      <S.Input
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        placeholder={placeholder}
-        isError={isError} value={value}
-        isFocused={isFocused}
-        onChangeText={onChangeText}
-      />
+      <PasswordBox>
+        <S.Input
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          placeholder={placeholder}
+          isError={isError} value={value}
+          isFocused={isFocused}
+          onChangeText={onChangeText}
+          secureTextEntry={passwordIsShow}
+        />
+        <ShowButton onPress={() => setPasswordIsShow(pre => !pre)}>
+          <Image source={passwordImage} style={{ width: 20, height: 20 }} />
+        </ShowButton>
+      </PasswordBox>
       {isError && !isFocused &&
         <S.ErrorContainer>
           <Image source={icon_input_error} style={{width: 17, height: 17}} />
@@ -46,3 +57,13 @@ export default function DefaultInput({ label, placeholder, isError, warningMessa
     </S.InputBox>
   )
 }
+
+const PasswordBox = styled.View`
+  position: relative;
+  flex-direction: row;
+  align-items: center;
+`
+
+const ShowButton = styled.Pressable`
+  left: -40px;
+`
