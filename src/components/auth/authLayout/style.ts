c75910a76d6a8a817c61ccba812b5d21c049d9be
abccba100 <abccba111307@gmail.com>
2026-02-05 17:04:34 +0900

    burn :: style.ts
    -Layout을 컴포넌트로 만들어 쓰던 style.ts 삭제

--- src/components/auth/authLayout/style.ts
+++
@@ -1,9 +0,0 @@
-import { colorStyle } from "@/styles/colorStyle";
-import styled from "styled-components/native";
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-export const LayoutContainer = styled(SafeAreaView)`
-  flex: 1;
-  padding: 36px 24px 52px; /* bottom쪽 padding은 디자이너한테 문의 해둔 상태 추후 변경 */
-  background-color: ${colorStyle.whiteBackground};
-`
