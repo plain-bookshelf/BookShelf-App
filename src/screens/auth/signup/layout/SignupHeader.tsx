af3063ea8fe46cf514a7518eaf286616e5e8f209
abccba100 <abccba111307@gmail.com>
2026-02-05 15:27:53 +0900

    fix :: SignupHeader touch space
    -이전 단계 버튼 클릭 가능 크기를 width: 10, height: 17에서 width: 24, height: 24로 변경

--- src/screens/auth/signup/layout/SignupHeader.tsx
+++ src/screens/auth/signup/layout/SignupHeader.tsx
@@ -4,19 +4,20 @@ import Typography from "@/components/common/typography/Typography";
 
 interface SignupHeaderProps {
   step: number,
+  maxStep: number,
   onPrev: () => void,
 }
 
-export default function SignupHeader({ step, onPrev }: SignupHeaderProps) {
+export default function SignupHeader({ step, maxStep, onPrev }: SignupHeaderProps) {
   const nowStep = Math.floor(step);
   
   return(
     <View style={{ width: '100%', flexDirection: "row", alignItems: "center", justifyContent: "space-between", height: 40, marginBottom: 16 }}>
-      <Pressable onPress={onPrev}>
-        <Image source={btn_previous_default} style={{ width: 10, height: 17}} />
+      <Pressable onPress={onPrev} >
+        <Image source={btn_previous_default} style={{ width: 24, height: 24}} />
       </Pressable>
       {/* TODO: 최대 step 지정 후 isShowStep 이런 식으로 step 보여주기 여부로 변경 필요함 */}
-      {step < 4 && <Typography children={`${nowStep} / 3`} font='medium16' color='defaultBlack' />}
+      {step < (maxStep + 1) && <Typography children={`${nowStep} / ${maxStep}`} font='medium16' color='defaultBlack' />}
     </View>
   )
 }
