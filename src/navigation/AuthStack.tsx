2e2e596cb0f9e39db48ce0f3d75e4e924bb800b6
abc100 <abccba111307@gmail.com>
2026-02-06 17:54:27 +0900

    feat :: AuthStack
    -RootNavigation 불필요한 코드 삭제
    -AuthStack 추가
    -App 불필요한 코드 삭제
    -KeyboardDismiss 컴포넌트는 필요한 Screen만 사용하도록 변경 필요

---
+++ src/navigation/AuthStack.tsx
@@ -0,0 +1,22 @@
+import AuthHomeScreen from "@/screens/auth/AuthHomeScreen";
+import FindIdScreen from "@/screens/auth/Id/FindIdScreen";
+import FindPasswordScreen from "@/screens/auth/password/FindPasswordScreen";
+import AdminSignupScreen from "@/screens/auth/signup/AdminSignupScreen";
+import UserSignupScreen from "@/screens/auth/signup/UserSignupScreen";
+import SignupRoleSelectScreen from "@/screens/auth/SignupRoleSelectScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthStack() {
+  return(
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="AuthHome" component={AuthHomeScreen} />
+      <Stack.Screen name="SignupRoleSelect" component={SignupRoleSelectScreen} />
+      <Stack.Screen name="UserSignup" component={UserSignupScreen} />
+      <Stack.Screen name="AdminSignup" component={AdminSignupScreen} />
+      <Stack.Screen name="FindPassword" component={FindPasswordScreen} />
+      <Stack.Screen name="FindName" component={FindIdScreen} />
+    </Stack.Navigator>
+  )
+}
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,29 +1,14 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import KeyboardDismiss from "@/components/common/KeyboardDismiss";
-import T from "@/screens/auth/SignupRoleSelectScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthStack from "./AuthStack";
 
 const Stack = createNativeStackNavigator();
 
 /** AuthStack과 MainStack 구별용 NativeStack */
 export default function RootNavigator() {
   return(
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={Temp}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  )
-}
-
-function Temp() {
-
-  return(
-    <>
-    <KeyboardDismiss>
-      <T />
-    </KeyboardDismiss>
-    </>
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
   )
 }
