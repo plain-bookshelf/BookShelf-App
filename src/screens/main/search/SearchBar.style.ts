20b345d3241ae6a8bd48c4b34f89d1a960a690dc
abc100 <abccba111307@gmail.com>
2026-03-04 13:51:39 +0900

    style :: SearchBar padding

--- src/screens/main/search/SearchBar.style.ts
+++ src/screens/main/search/SearchBar.style.ts
@@ -1,14 +1,13 @@
 import styled from "@emotion/native";
 
 export const Container = styled.View`
-  padding: 12px 20px 0px 0px;
+  padding: 12px 00px 0px 20px;
   flex-direction: row;
   align-items: center;
   gap: 16px;
 `
 
 export const SearchBar = styled.TextInput`
-  flex-direction: row;
   align-items: center;
   padding-left: 4px;
   height: 48px;
