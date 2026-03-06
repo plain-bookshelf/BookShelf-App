6c98fb09fe79556a32952615fafee01255a13e25
abc100 <abccba111307@gmail.com>
2026-03-06 09:36:18 +0900

    feat :: InfoCard component
    - InfoCard component 개발

---
+++ src/screens/main/profile/components/infoCard/style.ts
@@ -0,0 +1,13 @@
+import { colorStyle } from "@/styles/colorStyle";
+import styled from "@emotion/native";
+
+export const Container = styled.View`
+  width: 105px;
+  height: 92px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  padding: 0px 18px;
+  background-color: ${colorStyle.defaultWhite};
+`
