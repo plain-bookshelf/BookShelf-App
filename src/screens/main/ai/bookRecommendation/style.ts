92dde778fa10bb71a6b0b28dde909105bede1005
abc100 <abccba111307@gmail.com>
2026-03-08 16:02:01 +0900

    feat :: BookRecommendationScreen UI
    - BookRecommendationScreen UI 개발
    - TobTabs style 수정 필요

--- src/screens/main/ai/bookRecommendation/style.ts
+++ src/screens/main/ai/bookRecommendation/style.ts
@@ -1,6 +1,14 @@
+import { colorStyle } from "@/styles/colorStyle";
 import styled from "@emotion/native";
 
 export const Container = styled.View`
   flex: 1;
-  padding: 16px;
+  gap: 20px;
+  padding-top: 32px;
+  background-color: ${colorStyle.defaultWhite};
 `;
+
+export const TitleBox = styled.View`
+  padding: 0px 24px;
+  gap: 8px;
+`
