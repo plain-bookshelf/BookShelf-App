df2f3a2797693120f54828265eb03272c375474c
abc100 <abccba111307@gmail.com>
2026-03-12 20:03:32 +0900

    feat :: modal component
    - 기본적으로는 ProfileScreen 에서만 쓰이지만 추후 공통 컴포넌트로 분류될 가능성이 커 components/common 에 생성

---
+++ src/components/common/modal/DefaultModal.tsx
@@ -0,0 +1,56 @@
+import { Modal } from "react-native";
+import * as S from "./style";
+import Typography from "@/components/common/typography/Typography";
+
+interface DefaultModalProps {
+  visible: boolean;
+  title: string;
+  description?: string;
+  confirmLabel?: string;
+  cancelLabel?: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+export default function DefaultModal({
+  visible,
+  title,
+  description,
+  confirmLabel = "확인",
+  cancelLabel = "취소",
+  onConfirm,
+  onCancel,
+}: DefaultModalProps) {
+  return (
+    <Modal
+      transparent
+      animationType="slide"
+      visible={visible}
+      onRequestClose={onCancel}
+    >
+      <S.Backdrop>
+        <S.ModalContainer>
+          <S.TextBox>
+            <Typography font="bold20" color="defaultBlack" children={title} />
+            {description ? (
+              <Typography
+                font="regular16"
+                color="defaultBlack"
+                children={description}
+              />
+            ) : null}
+          </S.TextBox>
+          <S.ButtonRow>
+            <S.Button onPress={onCancel}>
+              <Typography font="medium16" color="defaultBlack" children={cancelLabel} />
+            </S.Button>
+            <S.Button onPress={onConfirm}>
+              <Typography font="medium16" color="modalButtonRed" children={confirmLabel} />
+            </S.Button>
+          </S.ButtonRow>
+        </S.ModalContainer>
+      </S.Backdrop>
+    </Modal>
+  );
+}
+
