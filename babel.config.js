b1d8d9a867373a368297626c49b9f9b54edbf36c
abccba100 <abccba111307@gmail.com>
2026-03-15 16:49:48 +0900

    Revert "Feat/login api"

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,6 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-      },
-    ],
-    [
       'module-resolver',
       {
         root: ['./'],
