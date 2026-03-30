424344dfc1291844ad6c11b37e181fe6d9e938e1
abc100 <abccba111307@gmail.com>
2026-03-31 04:15:40 +0900

    refactor :: using component
    - 파일 내에서 만들어 사용하는 것이 아닌 분리된 소속 검색 Input component를 import 하여 사용하도록 변경

--- src/screens/auth/signup/steps/LibraryStep.tsx
+++ src/screens/auth/signup/steps/LibraryStep.tsx
@@ -1,10 +1,7 @@
-import styled from "@emotion/native";
 import Typography from "@/components/common/typography/Typography";
 import ContentLayout from "@/components/layout/authLayout/AuthStepComponentLayout/ContentLayout";
-import icon_search_default from "@/assets/icon_search-_default.png"
-import { colorStyle } from "@/styles/colorStyle";
-import { fontStyle } from "@/styles/fontStyle";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import AffiliationInput from "@/components/common/Input/AffiliationInput";
 
 interface LibraryStepProps {
   value: string,
@@ -13,8 +10,6 @@ interface LibraryStepProps {
 }
 
 export default function LibraryStep({ value, onChange, setIsStepValid }: LibraryStepProps) {
-  const [isFocused, setIsFocused] = useState(false);
-
   useEffect(() => {
     if(value.trim().length !== 0){
       setIsStepValid(true)
@@ -25,61 +20,9 @@ export default function LibraryStep({ value, onChange, setIsStepValid }: Library
   }, [value])
 
     return(
-      <>
-        <ContentLayout>
-          <Typography children='소속 도서관 입력' font='medium28' color='defaultBlack' />
-          <InputContainer>
-            <Typography children='회원가입하고 책마루에 가입하세요' font='regular18' color='labelGray' />
-            <InputBox>
-              <Input
-                placeholder='소속 도서관 검색'
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                isFocused={isFocused}
-                value={value}
-                onChangeText={onChange}
-                placeholderTextColor={colorStyle.defaultGray}
-              />
-              <SearchIcon source={icon_search_default} />
-            </InputBox>
-          </InputContainer>
-        </ContentLayout>
-      </>
+      <ContentLayout>
+        <Typography children='소속 도서관 입력' font='medium28' color='defaultBlack' />
+        <AffiliationInput label='회원가입하고 책마루에 가입하세요' value={value} onChange={onChange} />
+      </ContentLayout>
     )
 }
-
-interface InputProps {
-  isError?: boolean,
-  isFocused?: boolean,
-}
-
-const InputContainer = styled.View`
-  flex-direction: column;
-  gap: 16px;
-`
-
-const InputBox = styled.View`
-  position: relative;
-  flex-direction: row;
-  align-items: center;
-`
-
-const Input = styled.TextInput<InputProps>`
-  color: ${( colorStyle.defaultBlack )};
-  ${( fontStyle.regular16 )};
-  width: 100%;
-  height: 48px;
-  background-color: ${( colorStyle.inputBarGray )};
-  border-width: 1px;
-  border-color: ${({ isFocused, isError }) => isFocused ? colorStyle.defaultBlack : isError ? colorStyle.defaultRed : colorStyle.inputBarGray};
-  border-radius: 8px;
-  align-items: center;
-  padding-left: 44px;
-`
-
-const SearchIcon = styled.Image`
-  position: absolute;
-  width: 20px;
-  height: 20px;
-  left: 20px;
-`
