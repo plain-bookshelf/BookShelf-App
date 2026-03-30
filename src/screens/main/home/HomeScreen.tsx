b29d9f2921bbf2f06be7e29993be6adcdea9422c
abc100 <abccba111307@gmail.com>
2026-03-30 21:55:17 +0900

    feat :: split popular, lates
    - 인기책들과 최신책들 useState로 상태 관리하도록 개발
    - 조건부 렌더링으로 인기책들과 친신책들 구분하도록 개발
    - 현재 MOCK_DATA를 추후 api 연동 진행 필요
    - 인기책들과 최신들 볼 수 있도록 개발 완료

--- src/screens/main/home/HomeScreen.tsx
+++ src/screens/main/home/HomeScreen.tsx
@@ -1,8 +1,10 @@
-import { Image } from "react-native";
+import { Image, Pressable } from "react-native";
 import banner_event_default from "@/assets/banner_event_default.png"
 import * as S from "./style"
 import Typography from "@/components/common/typography/Typography";
 import BookList from "@/components/common/bookList/BookList";
+import btn_next_type_books_arrow_default from "@/assets/btn_next-type-books-arrow-right_default.png"
+import { useState } from "react";
 
 export default function HomeScreen() {
   const MOCK_BOOKS = [
@@ -26,13 +28,23 @@ export default function HomeScreen() {
     { id: 18, image: require("@/assets/img_test-book_default.png") },
   ];
 
+  const [isPopularBooks, setIsPopularBooks] = useState(true);
+  const [popularBooks, setPopularBooks] = useState(MOCK_BOOKS);
+  const [latestBooks, setLatestBooks] = useState(MOCK_BOOKS);
+
   return(
     <S.Container>
       <Image source={banner_event_default} resizeMode="contain" style={{ width: "100%", height: 148 }} />
-      <S.TitleBox>
-        <Typography children='인기순 책 100권' font='semiBold22' color='defaultBlack' />
+      <S.TitleBox isPopularBooks={isPopularBooks}>
+        <Pressable onPress={() => setIsPopularBooks(true)}>
+          {!isPopularBooks && <Image source={btn_next_type_books_arrow_default} style={{ width: 28, height: 28, transform: [{ rotate: "180deg" }] }} />}  
+        </Pressable>
+        <Typography children={`${isPopularBooks ? "인기순" : "최신순"} 책 100권`} font='semiBold22' color='defaultBlack' />
+        <Pressable onPress={() => setIsPopularBooks(false)}>
+          {isPopularBooks && <Image source={btn_next_type_books_arrow_default} style={{ width: 28, height: 28 }} />}
+        </Pressable>
       </S.TitleBox>
-      <BookList bookList={MOCK_BOOKS} />
+      <BookList bookList={popularBooks} />
     </S.Container>
   )
 }
