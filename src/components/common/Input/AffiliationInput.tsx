5e08448a18adb68a120d6194e3f120f6ced960c2
abc100 <abccba111307@gmail.com>
2026-03-31 04:14:34 +0900

    feat :: AffiliationInput component
    - 기존에 회원가입에서만 사용되던 소속 입력 Input을 프로필 수정 화면에서도 쓸 수 있도록 컴포넌트로 분리

---
+++ src/components/common/Input/AffiliationInput.tsx
@@ -0,0 +1,70 @@
+import { colorStyle } from "@/styles/colorStyle";
+import styled from "@emotion/native";
+import Typography from "../typography/Typography";
+import { useState } from "react";
+import icon_search_default from "@/assets/icon_search-_default.png"
+import { fontStyle } from "@/styles/fontStyle";
+
+interface AffiliationInputProps {
+  label?: string,
+  value: string,
+  onChange: (text: string) => void,
+}
+
+export default function AffiliationInput({ label, value, onChange }: AffiliationInputProps) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <InputContainer>
+    {label && <Typography children={label} font='regular18' color='labelGray' />}
+    <InputBox>
+      <Input
+        placeholder='소속 도서관 검색'
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        isFocused={isFocused}
+        value={value}
+        onChangeText={onChange}
+        placeholderTextColor={colorStyle.defaultGray}
+      />
+      <SearchIcon source={icon_search_default} />
+    </InputBox>
+  </InputContainer>
+  )
+}
+
+interface InputProps {
+  isError?: boolean,
+  isFocused?: boolean,
+}
+
+const InputContainer = styled.View`
+  flex-direction: column;
+  gap: 16px;
+`
+
+const InputBox = styled.View`
+  position: relative;
+  flex-direction: row;
+  align-items: center;
+`
+
+const Input = styled.TextInput<InputProps>`
+  color: ${( colorStyle.defaultBlack )};
+  ${( fontStyle.regular16 )};
+  width: 100%;
+  height: 48px;
+  background-color: ${( colorStyle.inputBarGray )};
+  border-width: 1px;
+  border-color: ${({ isFocused, isError }) => isFocused ? colorStyle.defaultBlack : isError ? colorStyle.defaultRed : colorStyle.inputBarGray};
+  border-radius: 8px;
+  align-items: center;
+  padding-left: 44px;
+`
+
+const SearchIcon = styled.Image`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  left: 20px;
+`
