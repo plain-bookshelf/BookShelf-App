75d94b9abf8f39c169886550026fcd4592ac2fbe
abc100 <abccba111307@gmail.com>
2026-04-02 11:33:01 +0900

    feat :: initial props auto navigation
    - initail을 props로 받아서 사용함으로서 navigate 되도록 개발

--- src/navigation/tabs/MyBooksTopTab.tsx
+++ src/navigation/tabs/MyBooksTopTab.tsx
@@ -1,15 +1,22 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { MyBooksStackParamList } from "../type";
+import { MyBooksStackParamList, ProfileStackParamList } from "../type";
 import { colorStyle } from "@/styles/colorStyle";
 import BorrowedScreen from "@/screens/main/MyBooks/borrowed/BorrowedScreen";
 import OverdueScreen from "@/screens/main/MyBooks/overdue/OverdueScreen";
 import ReservedScreen from "@/screens/main/MyBooks/reserved/ReservedScreen";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 const TopTab = createMaterialTopTabNavigator<MyBooksStackParamList>();
 
-export default function MyBooksTopTabs() {
+type Props = NativeStackScreenProps<ProfileStackParamList, "MyBooks">;
+
+export default function MyBooksTopTabs({ route }: Props) {
+  const initialTab = route.params?.initialTab ?? "Borrowed";
+
   return (
     <TopTab.Navigator
+      key={initialTab}
+      initialRouteName={initialTab}
       screenOptions={{
         tabBarLabelStyle: { fontSize: 16, fontWeight: "500" },
         tabBarActiveTintColor: colorStyle.defaultBlack,
