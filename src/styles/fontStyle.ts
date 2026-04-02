43a727e273b3f76aec5ba15146895033a61169c1
abc100 <abccba111307@gmail.com>
2026-04-02 14:40:04 +0900

    fix :: font-weight duplication
    - 설치된 폰트에서 이미 font-weight까지 함께 제공하고 있는데 css 속성에서 다시 font-weight를 적용하는 버그 수정

--- src/styles/fontStyle.ts
+++ src/styles/fontStyle.ts
@@ -19,192 +19,155 @@ export const fontStyle = {
   bold24: `
     fontFamily: ${PRETENDARD.bold};
     fontSize: 24;
-    fontWeight: 700;
   `,
   bold22: `
     fontFamily: ${PRETENDARD.bold};
     fontSize: 22;
-    fontWeight: 700;
   `,
   bold20: `
     fontFamily: ${PRETENDARD.bold};
     fontSize: 20;
-    fontWeight: 700;
   `,
   bold18: `
     fontFamily: ${PRETENDARD.bold};
     fontSize: 18;
-    fontWeight: 700;
   `,
   bold16: `
     fontFamily: ${PRETENDARD.bold};
     fontSize: 16;
-    fontWeight: 700;
   `,
   semiBold24: `
     fontFamily: ${PRETENDARD.semiBold};
     fontSize: 24;
-    fontWeight: 600;
   `,
   semiBold22: `
     fontFamily: ${PRETENDARD.semiBold};
     fontSize: 22;
-    fontWeight: 600;
   `,
   semiBold20: `
     fontFamily: ${PRETENDARD.semiBold};
     fontSize: 20;
-    fontWeight: 600;
   `,
   semiBold18: `
     fontFamily: ${PRETENDARD.semiBold};
     fontSize: 18;
-    fontWeight: 600;
   `,
   semiBold16: `
     fontFamily: ${PRETENDARD.semiBold};
     fontSize: 16;
-    fontWeight: 600;
   `,
   semiBold14: `
     fontFamily: ${PRETENDARD.semiBold};
     fontSize: 14;
-    fontWeight: 600;
   `,
   semiBold12: `
     fontFamily: ${PRETENDARD.semiBold};
     fontSize: 12;
-    fontWeight: 600;
   `,
 
   medium28: `
     fontFamily: ${PRETENDARD.medium};
     fontSize: 28;
-    fontWeight: 500;
   `,
 
   medium24: `
     fontFamily: ${PRETENDARD.medium};
     fontSize: 24;
-    fontWeight: 500;
   `,
   medium20: `
     fontFamily: ${PRETENDARD.medium};
     fontSize: 20;
-    fontWeight: 500;
   `,
   medium18: `
     fontFamily: ${PRETENDARD.medium};
     fontSize: 18;
-    fontWeight: 500;
   `,
   medium16: `
     fontFamily: ${PRETENDARD.medium};
     fontSize: 16;
-    fontWeight: 500;
   `,
   medium14: `
     fontFamily: ${PRETENDARD.medium};
     fontSize: 14;
-    fontWeight: 500;
   `,
   medium12: `
     fontFamily: ${PRETENDARD.medium};
     fontSize: 12;
-    fontWeight: 500;
   `,
 
   regular24: `
     fontFamily: ${PRETENDARD.regular};
     fontSize: 24;
-    fontWeight: 400;
   `,
   regular20: `
     fontFamily: ${PRETENDARD.regular};
     fontSize: 20;
-    fontWeight: 400;
   `,
   regular18: `
     fontFamily: ${PRETENDARD.regular};
     fontSize: 18;
-    fontWeight: 400;
   `,
   regular16: `
     fontFamily: ${PRETENDARD.regular};
     fontSize: 16;
-    fontWeight: 400;
   `,
   regular14: `
     fontFamily: ${PRETENDARD.regular};
     fontSize: 14;
-    fontWeight: 400;
   `,
   regular12: `
     fontFamily: ${PRETENDARD.regular};
     fontSize: 12;
-    fontWeight: 400;
   `,
 
   tjMedium24: `
     fontFamily: ${TJ.medium};
     fontSize: 24;
-    fontWeight: 400;
   `,
   tjMedium20: `
     fontFamily: ${TJ.medium};
     fontSize: 20;
-    fontWeight: 400;
   `,
   tjMedium18: `
     fontFamily: ${TJ.medium};
     fontSize: 18;
-    fontWeight: 400;
   `,
   tjMedium16: `
     fontFamily: ${TJ.medium};
     fontSize: 16;
-    fontWeight: 400;
   `,
   tjMedium14: `
     fontFamily: ${TJ.medium};
     fontSize: 14;
-    fontWeight: 400;
   `,
   tjMedium12: `
     fontFamily: ${TJ.medium};
     fontSize: 12;
-    fontWeight: 400;
   `,
 
   tjBold24: `
     fontFamily: ${TJ.bold};
     fontSize: 24;
-    fontWeight: 700;
   `,
   tjBold20: `
     fontFamily: ${TJ.bold};
     fontSize: 20;
-    fontWeight: 700;
   `,
   tjBold18: `
     fontFamily: ${TJ.bold};
     fontSize: 18;
-    fontWeight: 700;
   `,
   tjBold16: `
     fontFamily: ${TJ.bold};
     fontSize: 16;
-    fontWeight: 700;
   `,
   tjBold14: `
     fontFamily: ${TJ.bold};
     fontSize: 14;
-    fontWeight: 700;
   `,
   tjBold12: `
     fontFamily: ${TJ.bold};
     fontSize: 12;
-    fontWeight: 700;
   `,
 } as const
 
