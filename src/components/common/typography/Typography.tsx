cac6710da2302665ef77655d1ab3b84b42665233
abc100 <abccba111307@gmail.com>
2026-04-09 01:40:58 +0900

    refactor :: Typography add css
    - numberOfLines 추가 텍스트 표시할 최대 줄 제한 속성 props에 추가
    - 추후 다른 css 속성이나 구조 정리가 필요함

--- src/components/common/typography/Typography.tsx
+++ src/components/common/typography/Typography.tsx
@@ -8,6 +8,7 @@ interface TypographyProps {
   color: ColorKey,
   decoration?: boolean,
   textAlign?: "left" | "center" | "right";
+  numberOfLines?: number;
 }
 
 /**
@@ -15,10 +16,11 @@ interface TypographyProps {
  * @param font fontStyle 에 있는 style 중 하나 선택
  * @param color colorStyle 에 있는 style 중 하나 선택
  * @param textAlign text 정렬 방향 선택
+ * @param numberOfLines text 줄 수 제한 설정
  * @returns 스타일 적용된 Text 생성
  */
-export default function Typography({ children, font, color, decoration = false, textAlign = "left" }: TypographyProps) {
+export default function Typography({ children, font, color, decoration = false, textAlign = "left", numberOfLines }: TypographyProps) {
   return(
-    <S.StyledText font={font} color={color} decoration={decoration} textAlign={textAlign}>{children}</S.StyledText>
+    <S.StyledText font={font} color={color} decoration={decoration} textAlign={textAlign} numberOfLines={numberOfLines}>{children}</S.StyledText>
   )
 }
