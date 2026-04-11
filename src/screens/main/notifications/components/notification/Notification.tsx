90211f0305141630cbc2d440ad833c32f4166fb9
abc100 <abccba111307@gmail.com>
2026-04-12 02:46:03 +0900

    feat :: Notification component
    - Notification component 개발

---
+++ src/screens/main/notifications/components/notification/Notification.tsx
@@ -0,0 +1,32 @@
+import { Shadow } from "react-native-shadow-2";
+import * as S from "./style";
+import Typography from "@/components/common/typography/Typography";
+
+interface NotificationProps {
+  title: string;
+  content: string;
+  isRead: boolean;
+}
+
+export default function Notification({ title, content, isRead }: NotificationProps) {
+  return (
+    <Shadow
+      stretch
+      distance={5}
+      startColor="rgba(0,0,0,0.25)"
+      endColor="rgba(0,0,0,0.02)"
+      offset={[0, 4]}
+      paintInside={false}
+      style={{ borderRadius: 8 }}
+      containerStyle={{
+        width: "100%",
+        opacity: isRead ? 0.5 : 1,
+      }}
+    >
+      <S.Surface>
+        <Typography font="medium16" color="defaultBlack" children={title} />
+        <Typography font="medium12" color="defaultGray" children={content} />
+      </S.Surface>
+    </Shadow>
+  );
+}
