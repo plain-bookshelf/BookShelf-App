811e965efa88424f829e48453eeac29699b3fabf
abc100 <abccba111307@gmail.com>
2026-04-12 04:15:33 +0900

    fix :: import path
    - 잘못된 경로 수정

--- src/navigation/stacks/NotificationStack.tsx
+++ src/navigation/stacks/NotificationStack.tsx
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NotificationStackParamList } from "../type";
 import NotificationsScreen from "@/screens/main/notifications/NotificationsScreen";
-import NotificationDetailScreen from "@/screens/main/notifications/NotificationDetailScreen";
+import NotificationDetailScreen from "@/screens/main/notificationDetail/NotificationDetailScreen";
 
 const Stack = createNativeStackNavigator<NotificationStackParamList>();
 
