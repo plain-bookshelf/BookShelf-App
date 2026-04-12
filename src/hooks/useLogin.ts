6a10ad1d1b42ea90b67a01302d55b6b3fecb7b36
abc100 <abccba111307@gmail.com>
2026-04-12 11:14:18 +0900

    refactor :: api hooks
    - react-query를 이용하여 커스텀 hook 구조 개선 및 스크린에서 개선된 코드를 사용하도록 변경

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,32 +1,21 @@
-import { useState, useCallback } from "react";
-import { login as loginApi } from "@/services/api/auth";
+import { useMutation } from "@tanstack/react-query";
+import { login } from "@/services/api/auth";
 import useAuthStore from "@/store/useAuthStore";
 import type { LoginRequest } from "@/types/auth";
+import type { LoginResponse } from "@/types/auth/login";
 
-export function useLogin() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-  const setTokens = useAuthStore((state) => state.setTokens);
+export const useLogin = () => {
+  return useMutation({
+    mutationFn: async(params: LoginRequest) => await login(params),
 
-  const login = useCallback(async (params: LoginRequest) => {
-    try {
-      setIsLoading(true);
-      setError(null);
+    onSuccess: async (data: LoginResponse) => {
+      const { accessToken, refreshToken } = data;
 
-      const res = await loginApi(params);
-      await setTokens(res.access_token, res.refresh_token);
-    } catch (e: any) {
-      setError(e?.response?.data?.message ?? "로그인에 실패했습니다.");
-      throw e;
-    } finally {
-      setIsLoading(false);
-    }
-  }, [setTokens]);
+      await useAuthStore.getState().setTokens(accessToken, refreshToken);
+    },
 
-  return {
-    login,
-    isLoading,
-    error,
-    clearError: () => setError(null),
-  };
-}
+    onError: (error) => {
+      console.error("로그인 실패", error);
+    },
+  });
+};
---
+++ src/hooks/useLogout.ts
@@ -0,0 +1,24 @@
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
+import { logout } from "@/services/api/auth";
+import useAuthStore from "@/store/useAuthStore";
+
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+
+  const handleLogout = async () => {
+    queryClient.clear();
+    await useAuthStore.getState().clearTokens();
+    /* TODO: 유저 정보 storage 만들면 여기서 초기화 함수 실행 */
+  }
+  
+  return useMutation({
+    mutationFn: logout,
+    onSuccess: handleLogout,
+    onError: (error) => {
+      console.error("로그아웃 실패", error);
+      handleLogout();
+    },
+  });
+};
+
+export default useLogout;
