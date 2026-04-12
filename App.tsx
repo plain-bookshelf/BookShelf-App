17583e0209ec1b780e03a621c6de9de5ab1f2680
abc100 <abccba111307@gmail.com>
2026-04-12 11:09:20 +0900

    refactor :: react-query init
    - react-query 사용을 위한 기본 세팅

--- App.tsx
+++ App.tsx
@@ -5,9 +5,11 @@ import {
   SafeAreaProvider,
 } from 'react-native-safe-area-context';
 import ImmersiveMode from "react-native-immersive-mode";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  const queryClient = new QueryClient();
 
   useEffect(() => {
     ImmersiveMode.fullLayout(true);
@@ -16,7 +18,9 @@ function App() {
 
   return (
     <SafeAreaProvider>
-      <RootNavigator />
+      <QueryClientProvider client={queryClient}>
+        <RootNavigator />
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
