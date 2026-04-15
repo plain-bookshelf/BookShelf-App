139359599fc15f8ce2a28d7453fe4e47237ee8b3
abc100 <abccba111307@gmail.com>
2026-04-15 16:53:16 +0900

    refactor :: add optional  recommandBooks
    - books에 접근 시 옵셔널 체이닝으로 접근히여 쿼리 로딩 중 정보가 undefined 더라도 에러 없이 undefined 반환

--- src/screens/main/ai/bookRecommendation/BookRecommendationScreen.tsx
+++ src/screens/main/ai/bookRecommendation/BookRecommendationScreen.tsx
@@ -15,7 +15,7 @@ export default function BookRecommendationScreen() {
         <Typography font="medium28" color="defaultBlack" children={`${userName}님을 위한 추천 책`} />
         <Typography font="regular18" color="labelGray" children={`${userName}님을 위해 이런 책을 준비했어요`} />
       </S.TitleBox>
-      <BookList bookList={data.books} />
+      <BookList bookList={data?.books} />
     </S.Container>
   )
 }
