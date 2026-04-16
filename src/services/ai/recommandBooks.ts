c84433661bd8bc1d67b6d445c16b953463b3222d
abc100 <abccba111307@gmail.com>
2026-04-17 04:04:43 +0900

    refactor :: add return type
    - 반환 타입 명시
    - 불필요한 import문 삭제

--- src/services/ai/recommandBooks.ts
+++ src/services/ai/recommandBooks.ts
@@ -1,8 +1,8 @@
-import useUserStore from "@/store/useUserStore";
 import { aiClient } from "./client";
 import { AI_BASE_URL } from "@env";
+import type { RecommandBooksResponse } from "@/types";
 
-export const getRecommandBooks = async (username: string) => {
+export const getRecommandBooks = async (username: string): Promise<RecommandBooksResponse> => {
   const res = await aiClient.get(`${AI_BASE_URL}recommend_books/test007@bookmaru.com`);
   return res.data;
 };
