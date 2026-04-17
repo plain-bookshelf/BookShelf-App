4c26c8b96a2173803666a547dc56d43e9b4acbcb
abc100 <abccba111307@gmail.com>
2026-04-17 10:07:51 +0900

    feat :: add Signup exception
    - 스탭 별로 api 요청 실패시 다음 단계로 넘어갈 수 없도록 변경

--- src/hooks/useSignupStepControl.ts
+++ src/hooks/useSignupStepControl.ts
@@ -16,10 +16,10 @@ interface UseSignupStepControlParams {
 
 export const useSignupStepControl = ({ step, maxStep, setStep, setStepValid, form }: UseSignupStepControlParams) => {
   const navigation = useNavigation<AuthNav>();
-  const { mutate: signup } = useSignup();
-  const { mutate: emailSend } = useEmailSend();
-  const { mutate: emailVerification } = useEmailVerification();
-
+  const { mutateAsync: signup } = useSignup();
+  const { mutateAsync: emailSend } = useEmailSend();
+  const { mutateAsync: emailVerification } = useEmailVerification();
+  
   const updateStepValid = (index: number, valid: boolean) => {
     setStepValid(prev => {
       const next = [...prev];
@@ -32,24 +32,38 @@ export const useSignupStepControl = ({ step, maxStep, setStep, setStepValid, for
     setStep(s => s - 1);
   };
 
-  const handleNext = () => {
+  const handleNext = async () => {
     if (step === 1) {
-      emailSend({
+      try {
+        await emailSend({
         email: form.email,
       });
+      } catch (error) {
+        console.error("이메일 전송 실패", error);
+        return;
+      }
     } else if (step === 2) {
-      emailVerification({
+      try {
+        await emailVerification({
         email: form.email,
         verification_code: form.verificationCode,
       });
+      } catch (error) {
+        console.error("이메일 인증 실패", error);
+        return;
+      }
     } else if (step === maxStep) {
-      signup({
+      try {
+        await signup({
         username: form.email,
         password: form.password,
         email: form.email,
         affiliation_name: form.library,
       });
-      return;
+      } catch (error) {
+        console.error("회원가입 실패", error);
+        return;
+      }
     } else if (step === maxStep + 1) {
       navigation.navigate('AuthHome');
       return;
