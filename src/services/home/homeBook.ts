59fc4c6f0aa5a2bf66d5aba63c54683b27b6ca76
abc100 <abccba111307@gmail.com>
2026-04-21 23:07:47 +0900

    fix :: homeBook api page
    - param의 page를 기본 1에서 0으로 수정

--- src/services/home/homeBook.ts
+++ src/services/home/homeBook.ts
@@ -6,7 +6,7 @@ const HOME_BASE = "main";
 export const  getHomeBook = async (
   bookFindType: BookFindType,
 ): Promise<HomeBookResponse> => {
-  const res = await client.get(`${HOME_BASE}/book${platformType}&bookFindType=${bookFindType}&page=1&size=99`);
+  const res = await client.get(`${HOME_BASE}/book${platformType}&bookFindType=${bookFindType}&page=0&size=99`);
   console.log(res.data);
   return res.data;
 };
