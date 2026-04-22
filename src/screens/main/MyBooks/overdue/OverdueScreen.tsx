cfb62b1f995b71dec4ba067fd4cd4010e9193aa6
abc100 <abccba111307@gmail.com>
2026-04-22 17:20:27 +0900

    feat :: lending hook
    - lending hook 개발 및 스크린 연결

--- src/screens/main/MyBooks/overdue/OverdueScreen.tsx
+++ src/screens/main/MyBooks/overdue/OverdueScreen.tsx
@@ -1,29 +1,19 @@
-import img_test_book_default from "@/assets/img_test-book_default.png";
 import * as S from "../style";
 import BookCard from "../components/bookCard/BookCard";
+import { useLendingInfo } from "@/hooks/useLendingInfo";
 
 export default function OverdueScreen() {
-  const MOCK_BOOKS = [
-    { id: 1, image: img_test_book_default, title: "오늘도 소심한 고양이", day: 10 },
-    { id: 2, image: img_test_book_default, title: "비가 내리는 날 소심한 고양이", day: 10 },
-    { id: 3, image: img_test_book_default, title: "소심한 고양이와 비가 내리는 날", day: 10 },
-    { id: 4, image: img_test_book_default, title: "소심한 고양이와 비가 내리는 날", day: 10 },
-    { id: 5, image: img_test_book_default, title: "소심한 고양이와 비가 내리는 날", day: 10 },
-    { id: 6, image: img_test_book_default, title: "소심한 고양이와 비가 내리는 날", day: 10 },
-    { id: 7, image: img_test_book_default, title: "소심한 고양이와 비가 내리는 날", day: 10 },
-    { id: 8, image: img_test_book_default, title: "소심한 고양이와 비가 내리는 날", day: 10 },
-    { id: 9, image: img_test_book_default, title: "소심한 고양이와 비가 내리는 날", day: 10 },
-    { id: 10, image: img_test_book_default, title: "소심한 고양이와 비가 내리는 날", day: 10 },
-  ];
+  const { overDueBooks } = useLendingInfo();
+
   return (
     <S.Container>
       <S.BookCardList>
-        {MOCK_BOOKS.map((book) => (
+        {overDueBooks?.map((book) => (
           <BookCard
-            key={book.id}
+            key={book.bookAffiliation_id}
             title={book.title}
-            info={`${book.day}일 연체됨`}
-            image={book.image}
+            info={`${book.overdue_date}일 연체됨`}
+            image={{ uri: book.book_image }}
             color="overdueRed"
           />
         ))}
