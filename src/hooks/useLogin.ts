5da5bf91a9f1ba1fc72cd480f55b6e5ffd721682
abc100 <abccba111307@gmail.com>
2026-05-01 17:46:06 +0900

    refactor :: chatSession api
    - chatSession hook을 만들어 관리하지 않고 login hook 안에서 직접 관리하도록 변경

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,6 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
 import { login } from "@/services/api/auth";
+import { postChatSession } from "@/services/ai/chat";
 import useAuthStore from "@/store/useAuthStore";
+import useUserStore from "@/store/useUserStore";
 import type { LoginRequest } from "@/types/auth";
 import type { LoginResponse } from "@/types/auth/login";
 
@@ -8,11 +10,19 @@ export const useLogin = () => {
   return useMutation({
     mutationFn: async(params: LoginRequest) => await login(params),
 
-    onSuccess: async (data: LoginResponse) => {
+    onSuccess: async (data: LoginResponse, variables: LoginRequest) => {
       const { access_token, refresh_token } = data.data;
       console.log(data.data);
 
       await useAuthStore.getState().setTokens(access_token, refresh_token);
+      useUserStore.getState().setUser({ username: variables.username });
+
+      try {
+        const chatSession = await postChatSession({ username: variables.username });
+        useUserStore.getState().updateUser({ chat_session_id: chatSession.id });
+      } catch (error) {
+        console.error("챗세션 생성 실패", error);
+      }
     },
 
     onError: (error) => {
